b9e0ec3a04f61578ff04a5dc54751e0bd0a91bdf
Yozu_Roo <114124424+Yozu-Roo@users.noreply.github.com>
2022-10-06 11:10:44 +0800

    Update node_predition_with_GCN.py
    添加了main函数，便与其他py文件调用

--- node_predition_with_GCN.py
+++ node_predition_with_GCN.py
@@ -137,42 +137,42 @@ args = {
     'epochs': 100,
 }
 
+if __name__ == '__main__':
+    #  调用OGB库中的数据集，本次实验为节点预测，选择数据集为ogbn-arxiv
+    dataset_name = 'ogbn-arxiv'
+    dataset = PygNodePropPredDataset(dataset_name, transform=T.ToSparseTensor())  # 将数据转换为稀疏矩阵存储
 
-#  调用OGB库中的数据集，本次实验为节点预测，选择数据集为ogbn-arxiv
-dataset_name = 'ogbn-arxiv'
-dataset = PygNodePropPredDataset(dataset_name, transform=T.ToSparseTensor())  # 将数据转换为稀疏矩阵存储
+    #  从数据集对象中提取数据部分，并设置邻接矩阵（对称矩阵）
+    data = dataset[0]
+    data.adj_t = data.adj_t.to_symmetric()
 
-#  从数据集对象中提取数据部分，并设置邻接矩阵（对称矩阵）
-data = dataset[0]
-data.adj_t = data.adj_t.to_symmetric()
+    #  数据的划分：训练、验证和测试
+    split_idx = dataset.get_idx_split()
+    train_idx = split_idx['train']
 
-#  数据的划分：训练、验证和测试
-split_idx = dataset.get_idx_split()
-train_idx = split_idx['train']
+    model = GCN(data.num_features, args['hidden_dim'],
+                dataset.num_classes, args['num_layers'],
+                args['dropout'])
+    evaluator = Evaluator(name='ogbn-arxiv')
 
-model = GCN(data.num_features, args['hidden_dim'],
-            dataset.num_classes, args['num_layers'],
-            args['dropout'])
-evaluator = Evaluator(name='ogbn-arxiv')
+    model.reset_parameters()  # 参数的随机初始化
 
-model.reset_parameters()  # 参数的随机初始化
+    optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'])
+    loss_fn = F.nll_loss  # 损失函数选择nll
 
-optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'])
-loss_fn = F.nll_loss  # 损失函数选择nll
+    best_model = None
+    best_valid_acc = 0
 
-best_model = None
-best_valid_acc = 0
-
-for epoch in range(1, 1 + args['epochs']):
-    loss = train(model, data, train_idx, optimizer, loss_fn)
-    result = test(model, data, split_idx, evaluator)
-    train_acc, valid_acc, test_acc = result
-    if valid_acc > best_valid_acc:
-        best_valid_acc = valid_acc
-        best_model = copy.deepcopy(model)
-    print(f'Epoch:{epoch:02d},'
-          f'Loss:{loss:.4f},'
-          f'Train:{100 * train_acc:.2f}%,'
-          f'Valid:{100 * valid_acc:.2f}%,'
-          f'Test:{100 * test_acc:.2f}%')
+    for epoch in range(1, 1 + args['epochs']):
+        loss = train(model, data, train_idx, optimizer, loss_fn)
+        result = test(model, data, split_idx, evaluator)
+        train_acc, valid_acc, test_acc = result
+        if valid_acc > best_valid_acc:
+            best_valid_acc = valid_acc
+            best_model = copy.deepcopy(model)
+        print(f'Epoch:{epoch:02d},'
+              f'Loss:{loss:.4f},'
+              f'Train:{100 * train_acc:.2f}%,'
+              f'Valid:{100 * valid_acc:.2f}%,'
+              f'Test:{100 * test_acc:.2f}%')
 
